7f25f4972681f0b253dc503b1979acd8333a1391
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-04 18:16:29 -0400

    created connection to database

---
+++ app.js
@@ -0,0 +1,21 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+  host: "localhost",
+
+***REMOVED***
+  port: 3306,
+
+***REMOVED***
+  user: "root",
+
+***REMOVED***
+  password: "",
+  database: "employee_trackerDB",
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
