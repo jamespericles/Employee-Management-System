1163031c58b215cb57c479ecacc65016d983312e
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-07 16:38:50 -0400

    created seeds.sql file to make testing easier

--- EMS-Schema.sql
+++ EMS-Schema.sql
@@ -14,7 +14,8 @@ id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL(20),
 department_id INT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
+FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
@@ -24,6 +25,8 @@ last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
 PRIMARY KEY (id)
+FOREIGN KEY(role_id) REFERENCES role(id)
+FOREIGN KEY(manager_id) REFERENCES employees(id)
 
 );
 
