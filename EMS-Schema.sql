b3a33cb635bc08cdc1624c3a5f547cfb226802af
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-07 15:38:04 -0400

    department delete function works properly

--- EMS-Schema.sql
+++ EMS-Schema.sql
@@ -5,47 +5,47 @@ USE employee_trackerDB;
 
 CREATE TABLE departments
 (
-    id INT NOT NULL
-    AUTO_INCREMENT,
-name VARCHAR
-    (30),
-PRIMARY KEY
-    (id)
-);
-
-    CREATE TABLE role
-    (
         id INT NOT NULL
         AUTO_INCREMENT,
-title VARCHAR
+  name VARCHAR
         (30),
-salary DECIMAL
-        (20),
-department_id INT,
-PRIMARY KEY
+  PRIMARY KEY
         (id)
 );
 
-        CREATE TABLE employee
+        CREATE TABLE role
         (
-            id INT NOT NULL
-            AUTO_INCREMENT,
+                id INT NOT NULL
+                AUTO_INCREMENT,
+title VARCHAR
+                (30),
+salary DECIMAL
+                (20),
+department_id INT,
+PRIMARY KEY
+                (id)
+);
+
+                CREATE TABLE employee
+                (
+                        id INT NOT NULL
+                        AUTO_INCREMENT,
 first_name VARCHAR
-            (30),
+                        (30),
 last_name VARCHAR
-            (30),
+                        (30),
 role_id INT,
 manager_id INT,
 PRIMARY KEY
-            (id)
+                        (id)
 
 );
 
-            SELECT *
-            FROM departments;
+                        SELECT *
+                        FROM departments;
 
-            SELECT *
-            FROM role;
+                        SELECT *
+                        FROM role;
 
-            SELECT *
-            FROM employee;
+                        SELECT *
+                        FROM employee;
