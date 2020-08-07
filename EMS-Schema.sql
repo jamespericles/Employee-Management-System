208ef357f0238bfaaf72c76e68cbb5e999b60a25
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-07 15:52:21 -0400

    rending roles now functions properly

--- EMS-Schema.sql
+++ EMS-Schema.sql
@@ -3,49 +3,32 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE departments
-(
-        id INT NOT NULL
-        AUTO_INCREMENT,
-  name VARCHAR
-        (30),
-  PRIMARY KEY
-        (id)
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
 );
 
-        CREATE TABLE role
-        (
-                id INT NOT NULL
-                AUTO_INCREMENT,
-title VARCHAR
-                (30),
-salary DECIMAL
-                (20),
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(20),
 department_id INT,
-PRIMARY KEY
-                (id)
+PRIMARY KEY(id)
 );
 
-                CREATE TABLE employee
-                (
-                        id INT NOT NULL
-                        AUTO_INCREMENT,
-first_name VARCHAR
-                        (30),
-last_name VARCHAR
-                        (30),
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-PRIMARY KEY
-                        (id)
+PRIMARY KEY (id)
 
 );
 
-                        SELECT *
-                        FROM departments;
+SELECT * FROM departments;
 
-                        SELECT *
-                        FROM role;
+SELECT * FROM role;
 
-                        SELECT *
-                        FROM employee;
+SELECT * FROM employee;
