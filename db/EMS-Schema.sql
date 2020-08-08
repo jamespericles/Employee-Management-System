29dcffdb6e40dba70b89975b8138800336fd4fc5
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-08 17:55:25 -0400

    working on updating employee's role

--- db/EMS-Schema.sql
+++ db/EMS-Schema.sql
@@ -24,8 +24,8 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-PRIMARY KEY (id)
-FOREIGN KEY(role_id) REFERENCES role(id)
+PRIMARY KEY (id),
+FOREIGN KEY(role_id) REFERENCES role(id),
 FOREIGN KEY(manager_id) REFERENCES employees(id)
 
 );
