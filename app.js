6f3825f153d7ebcc1b114e730f3e8c415e69abb4
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-08 22:50:43 -0400

    added function to change the department that a role belongs to

--- app.js
+++ app.js
@@ -395,6 +395,50 @@ function start() {
     );
   }
 
+  function roleUpdate() {
+    connection.query(
+      "SELECT * FROM role; SELECT * FROM departments",
+      (err, results) => {
+        if (err) throw err;
+        inquirer
+          .prompt([
+            {
+              name: "role",
+              type: "list",
+              message:
+                "What role would you like to assign to a new department?",
+              choices: () => results[0].map((result) => result.title),
+            },
+            {
+              name: "department",
+              type: "list",
+              message: "What department does this role belong to?",
+              choices: () => results[1].map((result) => result.name),
+            },
+          ])
+          .then((answers) => {
+            let roleID = results[0].filter(
+              (result) => result.title === answers.role
+            )[0].id;
+            let newDepartment = results[1].filter(
+              (result) => result.name === answers.department
+            )[0].id;
+            connection.query(`UPDATE role set department_id = ? WHERE id = ?`, [
+              newDepartment,
+              roleID,
+            ]);
+            console.log(
+              answers.role +
+                " now belongs to the " +
+                answers.department +
+                " department."
+            );
+            start();
+          });
+      }
+    );
+  }
+
   // This one's really cool, check this out...
   function employeeRole() {
     connection.query(
